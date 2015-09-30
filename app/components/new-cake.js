f2947a9f2eca1fbc32755b31f1c405e6f49e84ff
Archana Bongale & Jennifer Morkunas <student@epicodus.com>
2015-09-30 16:35:30 -0700

    img resizes to standard size

--- app/components/new-cake.js
+++ app/components/new-cake.js
@@ -8,12 +8,15 @@ export default Ember.Component.extend({
     },
 
     save1() {
+      // if (this.get('name') === "")
       var params = {
+        username: this.get('username'),
         name: this.get('name'),
         flavor: this.get('flavor'),
         frosting: this.get('frosting'),
         image: this.get('image'),
       };
+
       this.set('addNewCake', false),
       this.sendAction('save2', params);
     }
--- app/models/cake.js
+++ app/models/cake.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  username: DS.attr(),
   name: DS.attr(),
   flavor: DS.attr(),
   frosting: DS.attr(),
