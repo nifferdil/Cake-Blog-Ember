d6fe2d7b2f73345c4e4dac250ae5acb17dad430a
Archana Bongale & Jennifer Morkunas <student@epicodus.com>
2015-09-30 13:58:47 -0700

    add update functionality

--- app/components/cake-page.js
+++ app/components/cake-page.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+
+    update(cake, params) {
+      this.sendAction('update', cake, params);
+    },
+
     delete(cake) {
       if (confirm('Are you sure you want to delete this cake?')) {
         this.sendAction('destroyCake', cake);
---
+++ app/components/update-cake.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateCakeForm: false,
+  actions: {
+    updateCakeForm() {
+      this.set('updateCakeForm', true);
+    },
+    
+    update(cake) {
+      var params = {
+        name: this.get('name'),
+        flavor: this.get('flavor'),
+        frosting: this.get('frosting'),
+        image: this.get('image'),
+      };
+
+      this.set('updateCakeForm', false);
+      this.sendAction('update', cake, params);
+    }
+  }
+});
